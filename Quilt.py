c64a533487ca551c2dd96e429072df05597fca6e
ludovica <ellepannitto@gmail.com>
2017-04-21 21:02:51 +0200

    ver 0.2.2

--- Quilt.py
+++ Quilt.py
@@ -1,15 +1,30 @@
 #!/usr/bin/python
 
 def valid_alignment ( needle, haystack, posstart ):
+	
+	print "check if is a valid alignment"
+	print "needle", needle 
+	print "haystack", haystack
+	print "pos start", posstart 
+	
+	still_valid = False
+	
 	if posstart >=0 and posstart+len(needle) <= len(haystack):
 		j=0
 		still_valid = True
 		while j<len(needle) and still_valid:
+			print "j=",j
 			still_valid = False
 			if any ( x>=needle[j] for x in haystack[posstart+j] ):
 				still_valid = True
-		return still_valid
+			j += 1
+	
+	print still_valid, "\n\n"
+	raw_input ()
 
+	return still_valid
+	
+	
 def count_hor( mat ):
 		
 	r = []
@@ -37,26 +52,29 @@ def count_ver(mat):
 	r =  []
 	
 	for i in range(len(mat[0])):
-		maxlen = 0
+		curlis = []
 		l = 0
 	
 		line = [el[i] for el in mat]
 		
 		for x in line:
-			if x == 'x':
+			if x == '.':
 				l +=1
 			else:
+				if l>0:
+					curlis.append(l)
 				l=0
-				
-			if l > maxlen:
-				maxlen=l
-			
-		r.append(maxlen)		
+		
+		if l>0:
+			curlis.append(l)		
+		
+		r.append(curlis)
+		
 	
 	return r
 
 def parse_quilt ( quilt_str_array ):
-	ret = Quilt ( len(quilt_str) )
+	ret = Quilt ( len(quilt_str_array) )
 	
 	mat = [[ c for c in line.strip() ] for line in quilt_str_array ]
 	
@@ -73,30 +91,30 @@ class Quilt:
 		self.vholes = [ [ dim ] * dim ]
 		
 	
-	def all_the_possible_alignments ( needle, haystack ):
+	def all_the_possible_alignments ( self, needle, haystack ):
 		result_set = []
 		m = max (needle)
 		posmax = needle.index ( m )
-		for i in range ( haystack ):
-			if any ( x >= m for x in haystack[i] ) and valid_alignment ( needle, haystack, i-posmax ) :
+		for i in range ( len(haystack) ):
+			if any ( [ x >= m for x in haystack[i] ] ) and valid_alignment ( needle, haystack, i-posmax ) :
 				result_set.append ( i-posmax )
 		
 		return result_set
 		
 	
-	def candidate_positions ( l1, l2, top=-1 ):
+	def candidate_positions ( self, l1, l2, top=-1 ):
 		'''
-			finds the top candidate positions for a piece, given the maximum amount of contiguous squares in each row (l1) and in each col (l2). All the rotation and reflection of the piece are tried. 
+			finds the top candidate positions for a piece, given the maximum amount of contiguous squares in each row (l1) and in each col (l2). 
 			returns a list of all the positions in which that piece may be put. If a position is not returned, the piece cannot fit in that position for sure.
-			
-			
 		'''
 	
 		#TODO: implement score
-		#TODO: implement rotations and reflections
 		
-		return (x,y) for y in self.all_the_possible_alignments ( l2, self.vholes ) for x in self.all_the_possible_alignments ( l1, self.hholes ) 
+		valid_horizontal_alignment = self.all_the_possible_alignments ( l1, self.hholes )
+		valid_vertical_alignment = self.all_the_possible_alignments ( l2, self.vholes )
+		
+		return [ (x,y) for y in valid_vertical_alignment for x in valid_horizontal_alignment ] 
 
 if __name__ == "__main__":
 	q = parse_quilt ( open ("quilt_test").readlines() )
-	print q.candidate_positions ( [3,3], [1,2,2,1] )
+	print q.candidate_positions ( [1,2,2,1], [3,3] )
